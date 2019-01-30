763ff97aa7b2c35dbbbfe3634271a1d296f12021
Evan <eevan.mc@gmail.com>
2019-01-30 19:53:36 +0100

    Update and rename main.js to treant_config.js

--- treant_config.js
+++ treant_config.js
@@ -54,12 +54,6 @@
 						title: "Boucher",
 						contact: "1867 - 1914",
 					}
-                    /*childrenDropLevel: 1,
-                    children: [
-                        {
-                            image: "img/woodhouse.png"
-                        }
-                    ]*/
                 },
                 {
                     pseudo: true,
@@ -83,63 +77,3 @@
             ]
         }
     };
-
-/* Array approach
-    var config = {
-        container: "#collapsable-example",
-
-        animateOnInit: true,
-        
-        node: {
-            collapsable: true
-        },
-        animation: {
-            nodeAnimation: "easeOutBounce",
-            nodeSpeed: 700,
-            connectorsAnimation: "bounce",
-            connectorsSpeed: 700
-        }
-    },
-    malory = {
-        image: "img/malory.png"
-    },
-
-    lana = {
-        parent: malory,
-        image: "img/lana.png"
-    }
-
-    figgs = {
-        parent: lana,
-        image: "img/figgs.png"
-    }
-
-    sterling = {
-        parent: malory,
-        childrenDropLevel: 1,
-        image: "img/sterling.png"
-    },
-
-    woodhouse = {
-        parent: sterling,
-        image: "img/woodhouse.png"
-    },
-
-    pseudo = {
-        parent: malory,
-        pseudo: true
-    },
-
-    cheryl = {
-        parent: pseudo,
-        image: "img/cheryl.png"
-    },
-
-    pam = {
-        parent: pseudo,
-        image: "img/pam.png"
-    },
-
-    chart_config = [config, malory, lana, figgs, sterling, woodhouse, pseudo, pam, cheryl];
-
-*/
